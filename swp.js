bcec012f3317b34b989dbf68a5090e987ffc5214
Soldour <56756863+Soldour@users.noreply.github.com>
2019-12-01 23:01:04 +0200

    Update swp.js

--- swp.js
+++ swp.js
@@ -4,24 +4,24 @@ function swp(a,fI,sI){
 
       a[sI]=tmp
 }
-// let b=[4,6,7,9,1,0,2,5]
-// console.log(b)
+let b=[4,6,7,9,1,0,2,5]
+console.log(b)
  let a=[4,6,7,9,1,0,2,5]
-// console.log(b)
-// let fs= require("readline-sync");
-// let x=fs.questionFloat(`enter the firs index:` )
-// let y=fs.questionFloat(`enter the  seconnd index:` )
-// for(let i=0;i<1;i+=1)
-// {
+console.log(b)
+let fs= require("readline-sync");
+ let x=fs.questionFloat(`enter the firs index:` )
+let y=fs.questionFloat(`enter the  seconnd index:` )
+for(let i=0;i<1;i+=1)
+{
     
-//     swp(b,x,y)
+    swp(b,Math.abs(x),Math.abs(y)) // converts any input intiger in to positive 
 
-// }
-// console.log(b)
+}
+console.log(b)
 let n = a.length
-let x=n*0.5
+let k=n*0.5
 for(let i=0;i<n/2;i+=1)
 {
-    swp(a,i,x+i)
+    swp(a,i,k+i)
 }
 console.log(a)
